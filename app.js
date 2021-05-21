674d6c3cded9dfd17118124e4327e7892abbef7f
TheKillerz7 <mstudym13@gmail.com>
2021-05-22 05:26:20 +0700

    ready to deploy

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
 })
 
-app.use('/posting', send)
+app.post('/posting', send)
 
 
 app.listen(process.env.PORT || 5000)
