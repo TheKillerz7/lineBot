f6be13ba9e9b8aaefaaa2e16782d0a42d6f7af1c
TheKillerz7 <mstudym13@gmail.com>
2021-05-22 05:45:18 +0700

    ready to deploy

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const path = require('path');
 const app = express()
 const port = 3000
 
+app.use(express.urlencoded({
+  extended: true
+}))
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
 })
