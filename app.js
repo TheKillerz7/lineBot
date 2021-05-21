5d4188c54bdff2e8b3dbe4a2d3b4a8df343533e3
TheKillerz7 <mstudym13@gmail.com>
2021-05-22 06:29:03 +0700

    ready to deploy

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ const reply = (rt, payload) => {
       headers: {
           "Content-Type" : "application/json",
           Authorization: `Bearer qqYlHyvoss/pGBbV5XNerp2bbWgsTMt/s9xwYdvJWl1MvcPv3cwQWE+EJTTLZ7oU+hkLxVqe3M0W8K2y4XAnMHsg4rL2w77r+7pIb4oYOCPWQ14ShDzHSCca+i7+Gfuxn1uonvsQv184Lbi7ErWRMAdB04t89/1O/w1cDnyilFU=`
-
       },
       body: JSON.stringify({
           replyToken: rt,
