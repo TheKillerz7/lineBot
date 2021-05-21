eb50219e4d5239c76a1fc64a24af8d061625a483
TheKillerz7 <mstudym13@gmail.com>
2021-05-22 06:11:26 +0700

    ready to deploy

--- send.js
+++ send.js
@@ -9,8 +9,7 @@ const reply = (rt, payload) => {
         url: "https://api.line.me/v2/bot/message/reply",
         headers: {
             "Content-Type" : "application/json",
-            Authorization: `Bearer 
-            qqYlHyvoss/pGBbV5XNerp2bbWgsTMt/s9xwYdvJWl1MvcPv3cwQWE+EJTTLZ7oU+hkLxVqe3M0W8K2y4XAnMHsg4rL2w77r+7pIb4oYOCPWQ14ShDzHSCca+i7+Gfuxn1uonvsQv184Lbi7ErWRMAdB04t89/1O/w1cDnyilFU=`
+            Authorization: `Bearer qqYlHyvoss/pGBbV5XNerp2bbWgsTMt/s9xwYdvJWl1MvcPv3cwQWE+EJTTLZ7oU+hkLxVqe3M0W8K2y4XAnMHsg4rL2w77r+7pIb4oYOCPWQ14ShDzHSCca+i7+Gfuxn1uonvsQv184Lbi7ErWRMAdB04t89/1O/w1cDnyilFU=`
 
         },
         body: JSON.stringify({
