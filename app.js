6c1d85dfd5bdb66656f71002291c54f18383c055
TheKillerz7 <mstudym13@gmail.com>
2021-05-22 06:20:32 +0700

    ready to deploy

--- app.js
+++ app.js
@@ -12,7 +12,23 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
 })
 
-app.post('/posting', send)
+app.post('/posting', (req, res) => {
+  reply(req.body.events[0].replyToken, { type: "text", text: "dsa" })
+})
+
+const reply = (rt, payload) => {
+  app.post('https://api.line.me/v2/bot/message/reply', {
+      headers: {
+          "Content-Type" : "application/json",
+          Authorization: `Bearer qqYlHyvoss/pGBbV5XNerp2bbWgsTMt/s9xwYdvJWl1MvcPv3cwQWE+EJTTLZ7oU+hkLxVqe3M0W8K2y4XAnMHsg4rL2w77r+7pIb4oYOCPWQ14ShDzHSCca+i7+Gfuxn1uonvsQv184Lbi7ErWRMAdB04t89/1O/w1cDnyilFU=`
+
+      },
+      body: JSON.stringify({
+          replyToken: rt,
+          messages: [payload]
+      })
+  })
+}
 
 
 app.listen(process.env.PORT || 5000)
