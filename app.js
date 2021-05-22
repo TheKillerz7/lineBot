ead0b58a96dd95e6388e84ccd49e2b781adc1f77
TheKillerz7 <mstudym13@gmail.com>
2021-05-22 17:51:12 +0700

    ready to deploy

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ const reply = (rt, payload) => {
           Authorization: `Bearer qqYlHyvoss/pGBbV5XNerp2bbWgsTMt/s9xwYdvJWl1MvcPv3cwQWE+EJTTLZ7oU+hkLxVqe3M0W8K2y4XAnMHsg4rL2w77r+7pIb4oYOCPWQ14ShDzHSCca+i7+Gfuxn1uonvsQv184Lbi7ErWRMAdB04t89/1O/w1cDnyilFU=`
       },
       body: JSON.stringify({
-          replyToken: rt.events[0].replyToken,
-          messages: [payload]
+          "replyToken": rt.events[0].replyToken,
+          "messages": [{ type: "text", text: "dsa" }]
       })
   })
 }
