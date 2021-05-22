5a36ef99f2645903bdc39fe8de3cd6f5f7cb77be
TheKillerz7 <mstudym13@gmail.com>
2021-05-22 17:49:14 +0700

    ready to deploy

--- app.js
+++ app.js
@@ -14,9 +14,7 @@ app.get('/', (req, res) => {
 })
 
 app.post('/posting', (req, res) => {
-  reply(req.body.events?.[0].replyToken || "", { type: "text", text: "dsa" })
-
-  res.sendStatus(200)
+  reply(req.body, { type: "text", text: "dsa" })
 })
 
 const reply = (rt, payload) => {
@@ -26,7 +24,7 @@ const reply = (rt, payload) => {
           Authorization: `Bearer qqYlHyvoss/pGBbV5XNerp2bbWgsTMt/s9xwYdvJWl1MvcPv3cwQWE+EJTTLZ7oU+hkLxVqe3M0W8K2y4XAnMHsg4rL2w77r+7pIb4oYOCPWQ14ShDzHSCca+i7+Gfuxn1uonvsQv184Lbi7ErWRMAdB04t89/1O/w1cDnyilFU=`
       },
       body: JSON.stringify({
-          replyToken: rt,
+          replyToken: rt.events[0].replyToken,
           messages: [payload]
       })
   })
