08d68033b71de32411d47071794bd6b960afab50
TheKillerz7 <mstudym13@gmail.com>
2021-05-22 16:26:22 +0700

    ready to deploy

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 var send = require('./send');
 const path = require('path');
+const request = require('request-promise')
 const app = express()
 const port = 3000
 
@@ -19,10 +20,11 @@ app.post('/posting', (req, res) => {
 })
 
 const reply = (rt, payload) => {
-  app.get('https://api.line.me/v2/bot/channel/webhook/https://linetestwork.herokuapp.com/posting', {
+  request.post('https://api.line.me/v2/bot/channel/webhook/https://linetestwork.herokuapp.com/posting', {
       headers: {
           "Content-Type" : "application/json",
-          Authorization: `Bearer qqYlHyvoss/pGBbV5XNerp2bbWgsTMt/s9xwYdvJWl1MvcPv3cwQWE+EJTTLZ7oU+hkLxVqe3M0W8K2y4XAnMHsg4rL2w77r+7pIb4oYOCPWQ14ShDzHSCca+i7+Gfuxn1uonvsQv184Lbi7ErWRMAdB04t89/1O/w1cDnyilFU=`
+          Authorization: `Bearer 
+          qqYlHyvoss/pGBbV5XNerp2bbWgsTMt/s9xwYdvJWl1MvcPv3cwQWE+EJTTLZ7oU+hkLxVqe3M0W8K2y4XAnMHsg4rL2w77r+7pIb4oYOCPWQ14ShDzHSCca+i7+Gfuxn1uonvsQv184Lbi7ErWRMAdB04t89/1O/w1cDnyilFU=`
       },
       body: JSON.stringify({
           replyToken: rt,
--- send.js
+++ send.js
@@ -1,14 +1,27 @@
 const request = require('request-promise')
+const express = require('express')
+const app = express()
 
 const send = (req, res) =>{
-    reply(req.body.events[0].replyToken, { type: "text", text: "dsa" })
+    res.send(getUserIds());
+}
 
-    res.status(200)
+const getUserIds = () =>{
+    const ids = app.get("https://api.line.me/v2/bot/followers/ids", (req, res) => {
+        res.send(req)
+    }
+        // {
+        //     headers: {
+        //         Authorization: `Bearer qqYlHyvoss/pGBbV5XNerp2bbWgsTMt/s9xwYdvJWl1MvcPv3cwQWE+EJTTLZ7oU+hkLxVqe3M0W8K2y4XAnMHsg4rL2w77r+7pIb4oYOCPWQ14ShDzHSCca+i7+Gfuxn1uonvsQv184Lbi7ErWRMAdB04t89/1O/w1cDnyilFU=`
+        //     },
+        // }
+    )
+    return JSON.stringify(ids)
 }
 
 const reply = (rt, payload) => {
     request.post({
-        url: "https://api.line.me/v2/bot/message/reply",
+        url: "https://api.line.me/v2/bot/message/push",
         headers: {
             "Content-Type" : "application/json",
             Authorization: `Bearer qqYlHyvoss/pGBbV5XNerp2bbWgsTMt/s9xwYdvJWl1MvcPv3cwQWE+EJTTLZ7oU+hkLxVqe3M0W8K2y4XAnMHsg4rL2w77r+7pIb4oYOCPWQ14ShDzHSCca+i7+Gfuxn1uonvsQv184Lbi7ErWRMAdB04t89/1O/w1cDnyilFU=`
