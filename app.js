5b6d737f1e81cce8f3b4cb852f1a75f5f7f005a4
TheKillerz7 <mstudym13@gmail.com>
2021-05-22 18:11:13 +0700

    ready to deploy

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express')
 var send = require('./send');
 const path = require('path');
+const line = require('@line/bot-sdk');
 const request = require('request-promise')
 const app = express()
-const port = 3000
+
+const client = new line.Client({
+  channelAccessToken: 'qqYlHyvoss/pGBbV5XNerp2bbWgsTMt/s9xwYdvJWl1MvcPv3cwQWE+EJTTLZ7oU+hkLxVqe3M0W8K2y4XAnMHsg4rL2w77r+7pIb4oYOCPWQ14ShDzHSCca+i7+Gfuxn1uonvsQv184Lbi7ErWRMAdB04t89/1O/w1cDnyilFU='
+});
+
+const message = {
+  type: 'text',
+  text: 'Hello World!'
+};
 
 app.use(express.urlencoded({
   extended: true
@@ -14,7 +23,14 @@ app.get('/', (req, res) => {
 })
 
 app.post('/posting', (req, res) => {
-  reply(req.body, { type: "text", text: "dsa" })
+  client.replyMessage(req.body.events[0].replyToken, message)
+  .then(() => {
+    res.send('success')
+  })
+  .catch((err) => {
+    res.send(err)
+  });
+  // reply(req.body.events[0].replyToken, { type: "text", text: "dsa" })
 })
 
 const reply = (rt, payload) => {
@@ -24,8 +40,8 @@ const reply = (rt, payload) => {
           Authorization: `Bearer qqYlHyvoss/pGBbV5XNerp2bbWgsTMt/s9xwYdvJWl1MvcPv3cwQWE+EJTTLZ7oU+hkLxVqe3M0W8K2y4XAnMHsg4rL2w77r+7pIb4oYOCPWQ14ShDzHSCca+i7+Gfuxn1uonvsQv184Lbi7ErWRMAdB04t89/1O/w1cDnyilFU=`
       },
       body: JSON.stringify({
-          "replyToken": rt.events[0].replyToken,
-          "messages": [{ "type": "text", "text": "dsa" }]
+          replyToken: rt,
+          messages: [payload]
       })
   })
 }
