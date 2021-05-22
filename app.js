ded4369f8738d8844ac29765a51408cf9fc1832a
TheKillerz7 <mstudym13@gmail.com>
2021-05-22 07:00:35 +0700

    ready to deploy

--- app.js
+++ app.js
@@ -15,11 +15,11 @@ app.get('/', (req, res) => {
 app.post('/posting', (req, res) => {
   reply(req.body.events[0].replyToken, { type: "text", text: "dsa" })
 
-  res.status(200)
+  res.sendStatus(200)
 })
 
 const reply = (rt, payload) => {
-  app.post('https://api.line.me/v2/bot/message/reply', {
+  app.get('https://api.line.me/v2/bot/channel/webhook/https://linetestwork.herokuapp.com/posting', {
       headers: {
           "Content-Type" : "application/json",
           Authorization: `Bearer qqYlHyvoss/pGBbV5XNerp2bbWgsTMt/s9xwYdvJWl1MvcPv3cwQWE+EJTTLZ7oU+hkLxVqe3M0W8K2y4XAnMHsg4rL2w77r+7pIb4oYOCPWQ14ShDzHSCca+i7+Gfuxn1uonvsQv184Lbi7ErWRMAdB04t89/1O/w1cDnyilFU=`
