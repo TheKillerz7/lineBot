be556e955717c0c39ad234fe3be752d0c19db3ed
TheKillerz7 <mstudym13@gmail.com>
2021-05-22 16:35:00 +0700

    ready to deploy

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 })
 
 app.post('/posting', (req, res) => {
-  reply(req.body.events[0].replyToken || "", { type: "text", text: "dsa" })
+  reply(req.body.events?.[0].replyToken || "", { type: "text", text: "dsa" })
 
   res.sendStatus(200)
 })
@@ -23,8 +23,7 @@ const reply = (rt, payload) => {
   request.post('https://api.line.me/v2/bot/message/reply', {
       headers: {
           "Content-Type" : "application/json",
-          Authorization: `Bearer 
-          qqYlHyvoss/pGBbV5XNerp2bbWgsTMt/s9xwYdvJWl1MvcPv3cwQWE+EJTTLZ7oU+hkLxVqe3M0W8K2y4XAnMHsg4rL2w77r+7pIb4oYOCPWQ14ShDzHSCca+i7+Gfuxn1uonvsQv184Lbi7ErWRMAdB04t89/1O/w1cDnyilFU=`
+          Authorization: `Bearer qqYlHyvoss/pGBbV5XNerp2bbWgsTMt/s9xwYdvJWl1MvcPv3cwQWE+EJTTLZ7oU+hkLxVqe3M0W8K2y4XAnMHsg4rL2w77r+7pIb4oYOCPWQ14ShDzHSCca+i7+Gfuxn1uonvsQv184Lbi7ErWRMAdB04t89/1O/w1cDnyilFU=`
       },
       body: JSON.stringify({
           replyToken: rt,
