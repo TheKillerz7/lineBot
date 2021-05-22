ae1e50acf78dca7beeb77774bb11fa7cd94056f2
TheKillerz7 <mstudym13@gmail.com>
2021-05-22 21:47:49 +0700

    ready to deploy

--- app.js
+++ app.js
@@ -1,50 +1,58 @@
 const express = require('express')
-var send = require('./send');
-const path = require('path');
 const line = require('@line/bot-sdk');
 const request = require('request-promise')
 const app = express()
 
-const client = new line.Client({
-  channelAccessToken: 'qqYlHyvoss/pGBbV5XNerp2bbWgsTMt/s9xwYdvJWl1MvcPv3cwQWE+EJTTLZ7oU+hkLxVqe3M0W8K2y4XAnMHsg4rL2w77r+7pIb4oYOCPWQ14ShDzHSCca+i7+Gfuxn1uonvsQv184Lbi7ErWRMAdB04t89/1O/w1cDnyilFU='
-});
 
-const message = {
-  type: 'text',
-  text: 'Hello World!'
+//config for line middleware
+const config = {
+  channelAccessToken: 'qqYlHyvoss/pGBbV5XNerp2bbWgsTMt/s9xwYdvJWl1MvcPv3cwQWE+EJTTLZ7oU+hkLxVqe3M0W8K2y4XAnMHsg4rL2w77r+7pIb4oYOCPWQ14ShDzHSCca+i7+Gfuxn1uonvsQv184Lbi7ErWRMAdB04t89/1O/w1cDnyilFU=',
+  channelSecret: '6b618a4854126019742503a67d11e0d4'
 };
 
 app.use(express.urlencoded({
   extended: true
 }))
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
-})
 
-app.post('/posting', (req, res) => {
-  client.replyMessage(req.body.events[0].replyToken, message)
-  .then(() => {
-    res.send('success')
-  })
-  .catch((err) => {
-    res.send(err)
-  });
-  // reply(req.body.events[0].replyToken, { type: "text", text: "dsa" })
-})
-
-const reply = (rt, payload) => {
+//reply user
+app.post('/posting', line.middleware(config),  (req, res) => {
   request.post('https://api.line.me/v2/bot/message/reply', {
       headers: {
           "Content-Type" : "application/json",
           Authorization: `Bearer qqYlHyvoss/pGBbV5XNerp2bbWgsTMt/s9xwYdvJWl1MvcPv3cwQWE+EJTTLZ7oU+hkLxVqe3M0W8K2y4XAnMHsg4rL2w77r+7pIb4oYOCPWQ14ShDzHSCca+i7+Gfuxn1uonvsQv184Lbi7ErWRMAdB04t89/1O/w1cDnyilFU=`
       },
       body: JSON.stringify({
-          replyToken: rt,
-          messages: [payload]
+          replyToken: req.body.events[0].replyToken,
+          messages: [{
+            type: 'text',
+            text: 'Hello World!'
+          }]
       })
   })
-}
+})
+
+//push message
+// const ids = request.post({
+//   url: "https://api.line.me/v2/bot/followers/ids",
+//   headers: {
+//       Authorization: `Bearer qqYlHyvoss/pGBbV5XNerp2bbWgsTMt/s9xwYdvJWl1MvcPv3cwQWE+EJTTLZ7oU+hkLxVqe3M0W8K2y4XAnMHsg4rL2w77r+7pIb4oYOCPWQ14ShDzHSCca+i7+Gfuxn1uonvsQv184Lbi7ErWRMAdB04t89/1O/w1cDnyilFU=`
+//   }
+// })
+
+//push message
+// request.post({
+//   url: "https://api.line.me/v2/bot/message/push",
+//   headers: {
+//       "Content-Type" : "application/json",
+//       Authorization: `Bearer qqYlHyvoss/pGBbV5XNerp2bbWgsTMt/s9xwYdvJWl1MvcPv3cwQWE+EJTTLZ7oU+hkLxVqe3M0W8K2y4XAnMHsg4rL2w77r+7pIb4oYOCPWQ14ShDzHSCca+i7+Gfuxn1uonvsQv184Lbi7ErWRMAdB04t89/1O/w1cDnyilFU=`
+
+//   },
+//   body: JSON.stringify({
+//       to: 'Uf6a475f2e75abee2a91d043da265705d',
+//       messages: [payload]
+//   })
+// })
 
 
 app.listen(process.env.PORT || 5000)
--- send.js
+++ send.js
@@ -1,21 +1,19 @@
 const request = require('request-promise')
-const express = require('express')
-const app = express()
 
 const send = (req, res) =>{
-    res.send(getUserIds());
+    reply('s', {
+        type: 'text',
+        text: 'Hello dsadsaWorld!'
+      })
 }
 
 const getUserIds = () =>{
-    const ids = app.get("https://api.line.me/v2/bot/followers/ids", (req, res) => {
-        res.send(req)
-    }
-        // {
-        //     headers: {
-        //         Authorization: `Bearer qqYlHyvoss/pGBbV5XNerp2bbWgsTMt/s9xwYdvJWl1MvcPv3cwQWE+EJTTLZ7oU+hkLxVqe3M0W8K2y4XAnMHsg4rL2w77r+7pIb4oYOCPWQ14ShDzHSCca+i7+Gfuxn1uonvsQv184Lbi7ErWRMAdB04t89/1O/w1cDnyilFU=`
-        //     },
-        // }
-    )
+    const ids = request.get({
+        url: "https://api.line.me/v2/bot/followers/ids",
+        headers: {
+            Authorization: `Bearer qqYlHyvoss/pGBbV5XNerp2bbWgsTMt/s9xwYdvJWl1MvcPv3cwQWE+EJTTLZ7oU+hkLxVqe3M0W8K2y4XAnMHsg4rL2w77r+7pIb4oYOCPWQ14ShDzHSCca+i7+Gfuxn1uonvsQv184Lbi7ErWRMAdB04t89/1O/w1cDnyilFU=`
+        },
+    })
     return JSON.stringify(ids)
 }
 
@@ -28,7 +26,7 @@ const reply = (rt, payload) => {
 
         },
         body: JSON.stringify({
-            replyToken: rt,
+            to: 'Uf6a475f2e75abee2a91d043da265705d',
             messages: [payload]
         })
     })
